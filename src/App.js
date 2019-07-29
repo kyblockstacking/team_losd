09ad877b22d27786fc4ce1805fa1ed7b30ba1f27
knyg <kevin1992yang@gmail.com>
2019-07-29 00:49:08 -0700

    add initial 404

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.scss";
 
 // components
 import NavBar from "./Components/NavBar";
 import Hero from "./Components/Hero";
 
+import NoMatch_404 from "./Components/404";
+
 function App() {
   return (
     <div className='App'>
-      <NavBar />
-      <Hero />
+      <Router>
+        <Route path='/' component={NavBar} />
+        <Switch>
+          <Route exact path='/' component={Hero} />
+          <Route component={NoMatch_404} />
+        </Switch>
+      </Router>
     </div>
   );
 }
