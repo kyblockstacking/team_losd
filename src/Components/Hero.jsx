cef5931fbb1ddf15164eea407c5a0da46e371bc2
knyg <kevin1992yang@gmail.com>
2019-08-24 21:21:02 -0700

    some hero work

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "../Stylesheets/Hero.scss";
 
 const Hero = () => {
-  return <div className='hero' />;
+  return (
+    <div className='hero'>
+      <div className='hero-header'>
+        <h1 className='hero-header-name'>Team LOSD</h1>
+        <h2 className='hero-header-slogan'>Squad Up!</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
