9dea21ae18eb19a2b6b95100e95c25be1f0f48a5
knyg <kevin1992yang@gmail.com>
2019-08-24 19:29:23 -0700

    add colors to readme for ref

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ notes and stuff
 colors:
 
 -#EF798A light coral
+
 -#F7A9A8 pink sherbet
+
 -#613F75 cyber grape
+
 -#E5C3D1 dust storm
+
 -#988B8E mountbatten pink
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../Stylesheets/Hero.scss";
 
 const Hero = () => {
-  return <div className='test' />;
+  return <div className='hero' />;
 };
 
 export default Hero;
