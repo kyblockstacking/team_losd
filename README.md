ad57494fe4c2b6c521b709f1418c9cff0683d568
knyg <kevin1992yang@gmail.com>
2019-08-24 21:35:33 -0700

    hero work

--- README.md
+++ README.md
@@ -3,19 +3,30 @@
 colors:
 
 -#00AEFF starcraft blue
+
 -rgba(0, 174, 255, 1)
 
+<hr>
 -#EF798A light coral
+
 -rgba(239, 121, 138, 1)
 
+<hr>
 -#F7A9A8 pink sherbet
+
 -rgba(247, 169, 168, 1)
 
+<hr>
 -#613F75 cyber grape
+
 -rgba(97, 63, 117, 1)
 
+<hr>
 -#E5C3D1 dust storm
+
 -rgba(229, 195, 209, 1)
 
+<hr>
 -#988B8E mountbatten pink
+
 -rgba(152, 139, 142, 1)
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,10 @@ const Hero = () => {
     <div className='hero'>
       <div className='hero-header'>
         <h1 className='hero-header-name'>Team LOSD</h1>
-        <h2 className='hero-header-slogan'>Squad Up!</h2>
+        {/* <h2 className='hero-header-slogan'>Squad Up!</h2> */}
+        <h3 className='hero-header-info'>
+          The original North American Squadron Tower Defence clan
+        </h3>
       </div>
     </div>
   );
