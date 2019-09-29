7b7b2ea0f3e1c4fa4b70dccb17227bf40618a69b
knyg <kevin1992yang@gmail.com>
2019-09-28 20:03:52 -0700

    adjust size of game images

--- src/Components/GamesWePlay.jsx
+++ src/Components/GamesWePlay.jsx
@@ -11,20 +11,24 @@ import { Card } from "@material-ui/core";
 // Assets
 import Diablo3 from "../Assets/Games_We_Play/diablo3.jpg";
 import Starcraft2 from "../Assets/Games_We_Play/starcraft2.jpg";
-import Rust from "../Assets/Games_We_Play/rust.png";
+import Rust from "../Assets/Games_We_Play/rust.jpg";
+import BrawlStars from "../Assets/Games_We_Play/brawlstars.jpg";
 
 const GamesWePlay = () => {
   return (
     <>
       <Container className='games-we-play-container'>
         <Card className='game-image-card'>
-          <CardMedia image={Diablo3} className='game-image'></CardMedia>
+          <CardMedia image={Starcraft2} className='game-image' />
         </Card>
         <Card className='game-image-card'>
-          <CardMedia image={Starcraft2} className='game-image'></CardMedia>
+          <CardMedia image={Diablo3} className='game-image' />
         </Card>
         <Card className='game-image-card'>
-          <CardMedia image={Rust} className='game-image'></CardMedia>
+          <CardMedia image={Rust} className='game-image' />
+        </Card>
+        <Card className='game-image-card'>
+          <CardMedia image={BrawlStars} className='game-image' />
         </Card>
       </Container>
     </>
