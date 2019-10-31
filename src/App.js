63b108f325e77311ea233688a155070d5e3a6c20
Kevin Yang <kevin@711media.tv>
2019-10-30 19:03:22 -0700

    add in navbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 
-// components
-// import NavBar from "./Components/NavBar";
-
-// Home Page Comoponent
+// Components
+import NavBar from "./Components/NavBar";
 import HomePage from "./Pages/HomePage";
 
 import NoMatch_404 from "./Components/404";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
-        {/* <Route path='/' component={NavBar} /> */}
+        <Route path="/" component={NavBar} />
         <Switch>
-          <Route exact path='/' component={HomePage} />
+          <Route exact path="/" component={HomePage} />
           <Route component={NoMatch_404} />
         </Switch>
       </Router>
